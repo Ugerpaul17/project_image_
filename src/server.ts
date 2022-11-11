ab646e4066d347ea46e4658aab507d635d86770e
Ugerpaul17 <ugerpaul5@gmail.com>
2022-11-11 19:39:44 +0300

    add type to the variables

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -29,8 +29,9 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
- app.get("/filteredimage", async (req, res) => {
-  let { image_url } = req.query;
+ app.get("/filteredimage", async (req: Request, res: Response) => {
+
+  let  image_url:string = req.query;
 
   if (!image_url) {
     return res.status(400).send(`image_url is required`);
@@ -50,7 +51,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image");
   } );
   
